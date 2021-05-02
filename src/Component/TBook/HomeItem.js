97d24672eb637c3e19e00c93b8972f787097ea29
The Ngoc <ntngoc229@gmail.com>
2021-05-02 14:15:01 +0700

    update

--- src/Component/TBook/HomeItem.js
+++ src/Component/TBook/HomeItem.js
@@ -9,14 +9,13 @@ export default class HomeItem extends Component {
         return (
             <section className="container-fluid">
                  <div className="row">
-                     <div className="col-9">
+                     <div className="col">
                      <div class="card-deck">
                         <img class="card-img-top" src={this.props.image} alt="Card image cap" />
                         <div class="card-body">
                              <h5> <a href={this.props.link} class="card-title" target="_blank">{this.props.Title}</a></h5>
                             <p class="card-text card-text-desscription">{this.props.Description}</p>
                             <p class="card-title card-time">{this.props.time}</p>
-
                                 <span>
                                 <a href={this.props.stockLink} class="card-title" target="_blank" rel="noopener">{this.props.stock}</a>
                                 </span>
@@ -24,16 +23,16 @@ export default class HomeItem extends Component {
                         </div>
                         </div>                    
                         </div>
-                        <div className="col-3">
-                            {/* <h3>Tin Chính</h3>
+                        {/* <div className="col-3">
+                            <h3>Tin Chính</h3>
                             <ul>
                                 <li>
                                     <div className="thumb">
                                         <img src={this.props.image}></img>
                                     </div>
                                 </li>
-                            </ul> */}
-                        </div>
+                            </ul>
+                        </div> */}
                      </div>
             </section>
             
