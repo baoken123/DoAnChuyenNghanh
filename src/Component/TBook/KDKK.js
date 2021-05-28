f40c760b0531235749bda196fe8cd7e06428455d
The Ngoc <ntngoc229@gmail.com>
2021-05-28 19:53:48 +0700

    update

--- src/Component/TBook/KDKK.js
+++ src/Component/TBook/KDKK.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {List, notification } from "antd";
+import {List, notification ,Pagination ,Spin } from "antd";
 
  function KDKK() {
   const [data, setData] = useState([]);
-
+  const [current, setCurrent] = useState(1);
+  const [isShowLoading, setIsShowLoading] = useState(false);
   // Tương tự như componentDidMount và componentDidUpdate:
   useEffect(() => {
       axios.get('https://quanlyminhchungdaotaoapi.herokuapp.com/api/v1/lay-du-lieu/vietstock')
@@ -12,7 +13,14 @@ import {List, notification } from "antd";
           setData(respone.data.ListObject)
       })
   },[]);
-
+  useEffect(() => {
+    setIsShowLoading(true);
+    axios.get(`https://quanlyminhchungdaotaoapi.herokuapp.com/api/v1/lay-du-lieu/vietstockwithpage?pageNumber=${current}`)
+      .then(respone => {
+          setData(respone.data.ListObject)
+          console.log(respone.data);
+      }).finally(()=>{setIsShowLoading(false);})
+  }, [current])
   return (
     <div>
       <List
@@ -40,8 +48,34 @@ import {List, notification } from "antd";
           </List.Item>
         )}
       />
-      
+      <Pagination current={current} onChange={(page)=>setCurrent(page)} defaultCurrent={1} total={50} />
+      {isShowLoading && <div
+              className="cLoading"
+              style={{
+                height: "100vh",
+                width: "100%",
+                display: "block",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                zIndex: 999,
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+              }}
+            >
+              <Spin
+                size="large"
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  zIndex: 999,
+                }}
+              />
+            </div>}
     </div>
+    
   );
 }
 export default KDKK;
